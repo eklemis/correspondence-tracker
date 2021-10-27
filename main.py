c0c4f9313df6af6d806205fefc3d94e6fcf070d9
eklemis <eklemis.ndun@gmail.com>
2021-10-27 15:09:55 +0800

    basic kivy  user interface start to use

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+
 from notification import Notification
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class WelcomeScreen(Screen):
+    pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class FirstScreen(Screen):
+    pass
 
+
+class SecondScreen(Screen):
+    pass
+
+
+class ScreenManager(ScreenManager):
+    pass
+
+class BarcodeGeneratorForm(GridLayout):
+    pass
+class BarcodesPage(GridLayout):
+    pass
+
+class MainFront(GridLayout):
+    pass
+
+class CorrManagerApp(App):
+    title = 'Sponsorship Correspondence Manager'
+    def build(self):
+        return MainFront()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    notif = Notification("Hi,\nYou have 5 correspondence almost late (3 days left).\nPlease update their status!")
-    notif.show_notif()
+    CorrManagerApp().run()
+    #notifikasi = Notification("Hi,\nYou have 5 correspondence almost late (3 days left).\nPlease update their status!")
+    #notifikasi.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- notification.py
+++ notification.py
@@ -3,6 +3,6 @@ from win10toast import ToastNotifier
 class Notification:
     def __init__(self, message):
         self.message = message
-    def show_notif(self):
+    def show(self):
         toast = ToastNotifier()
         toast.show_toast("Sponsorship Corr Tracker",self.message, duration=20,icon_path="icon.ico")
