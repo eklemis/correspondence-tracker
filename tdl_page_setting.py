da640d23803c84804a9437c8db2eb70297eb9a18
eklemis <eklemis.ndun@gmail.com>
2021-11-12 13:47:21 +0800

    correct page settings for filled TDL pages

--- tdl_page_setting.py
+++ tdl_page_setting.py
@@ -1,7 +1,7 @@
-start_top = 15
-space_vertical = 12
-start_left = 36
-space_horizontal = 75
+start_top = 14
+space_vertical = 13
+start_left = 42
+space_horizontal = 73
 
 font = "NunitoSans400"
 font_source = r"Nunito_Sans\NunitoSans-Regular.ttf"
